99ae5defa6d9be7f416d03aa1c715d0a4b9a0b26
Nar Bahadur dhami <dhamiparkash660@gmail.com>
2022-12-13 16:50:10 +0545

    insertion

--- insertion.cpp
+++ insertion.cpp
@@ -2,14 +2,27 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
 timefunction()
 {
-	time_t seconds;
-	
-	seconds = time(NULL);
-	printf("Seconds since January 1, 1970 = %ld\n", seconds);
+    clock_t start, stop;
+    int i;
+    start = clock();
+    for(i=0; i<2000;i++)
+    {
+        printf("%d", (i*1)+(1^4));
+    }
+    printf("\n\n");
+    stop = clock();
+
+    //(double)(stop - start) / CLOCKS_PER_SEC
+  printf("before sorting=");
+    printf("%6.3f\n", start);
+   printf("after sorting=");
+
+    printf("%6.3f", stop);
+    return 0;
 }
+
 
 // Function to sort an array using
 // insertion sort
@@ -34,21 +47,36 @@ void insertionSort(int arr[], int n)
 	}
 }
 
+// A utility function to print an array
+// of size n
+void printArray(int arr[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
 
-
-
+// Driver code
 int main()
 {
-	float t,t1,t2;
-	int arr[] = { 12, 11, 13, 5, 6 };
+	int arr[] = { 50,40,35,30,25,20,15,17,12,115,11,10,3};
 	int N = sizeof(arr) / sizeof(arr[0]);
 
-    t1=timefunction();
-	insertionSort(arr, N);
-	t2=timefunction();
 	
-    t=t1-t2;
-	cout<<"timecomplexity="<<t<<  " seconds";
+	float t,t1,t2;
+	
+	 cout<<"before sorting";
+	 t1=timefunction();
+     insertionSort(arr, N);
+	 cout<<"after sorting";
+     t2=timefunction();
+	
+	t=t2-t1;
+	cout<<endl<<"timecomplexity= "<<t<<" milisecond\n";
+     
+	printArray(arr, N);
+
 	return 0;
 }
 
